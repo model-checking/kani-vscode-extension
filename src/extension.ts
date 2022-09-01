2a0260cb9381e88b4074b15d4ad80efb7e53f7bb
Jaisurya Nanduri <91620234+jaisnan@users.noreply.github.com>
2022-09-01 08:40:35 -0400

    Refactor main flow outside the activation function (#1)
    * Clean up default comments
    
    * Refactor functionality outside the activate function

--- src/extension.ts
+++ src/extension.ts
@@ -1,56 +1,39 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "Kani" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
 	const runKani = vscode.commands.registerCommand('Kani.runKani', async () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-
-		const answer = await vscode.window.showInformationMessage('Run Kani?', "yes", "no");
-		if(answer === "yes"){
-			console.log("Running Kani");
-			runCommand("kani");
-		}
-		else{
-			console.log("not running kani");
-		}
-
+		showInformationMessage('Kani.runKani');
 	});
-
 	const runcargoKani = vscode.commands.registerCommand('Kani.runcargoKani', async () => {
-
 		//TODO: Always point to the root of the cargo crate (or source file) before running cargo kani
-		const answer = await vscode.window.showInformationMessage('Run Cargo Kani?', "yes", "no");
-		if(answer === "yes"){
-			console.log("Running Cargo Kani");
-			runCommand("cargo kani");
-		}
-		else{
-			console.log("not running cargo kani");
-		}
+		showInformationMessage('Kani.runcargoKani');
 
 	});
 
 	context.subscriptions.push(runKani);
 	context.subscriptions.push(runcargoKani);
-
-	// Run cargo clean once done
+	//TODO: Run cargo clean once done
 
 }
 
+async function showInformationMessage(message: string): Promise<void> {
+
+	const command: string = message === "Kani.runcargoKani" ? "cargo kani" : "kani";
+
+	const answer = await vscode.window.showInformationMessage(`Run ${command}?`, "yes", "no");
+	if(answer === "yes"){
+		console.log(`Running ${command}`);
+		runCommand(command);
+	}
+	else{
+		console.log(`Exiting ${command} extension`);
+	}
+}
+
 function runCommand(command: string): void {
-	// const kaniCommand: string = "cargo kani";
 	let finalCommand = ``;
 
 	// Detect source file
@@ -65,8 +48,9 @@ function runCommand(command: string): void {
 		finalCommand = `${command}`;
 	}
 
-	terminal.show();
 	terminal.sendText(finalCommand);
+	terminal.show();
+
 }
 
 // this method is called when your extension is deactivated
