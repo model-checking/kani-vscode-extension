aaed30adf323182c4313fb8a9e010e0fa554dedb
jaisnan <jaisnan@amazon.com>
2022-12-15 22:12:55 +0000

    rename callback functions in index.ts

--- src/test/suite/index.ts
+++ src/test/suite/index.ts
@@ -14,10 +14,10 @@ export function run(): Promise<void> {
 
 	const testsRoot = path.resolve(__dirname, '..');
 
-	return new Promise((c, e) => {
+	return new Promise((resolve, reject) => {
 		glob('**/**.test.js', { cwd: testsRoot }, (err, files) => {
 			if (err) {
-				return e(err);
+				return reject(err);
 			}
 
 			// Add files to the test suite
@@ -27,14 +27,14 @@ export function run(): Promise<void> {
 				// Run the mocha test
 				mocha.run((failures) => {
 					if (failures > 0) {
-						e(new Error(`${failures} tests failed.`));
+						reject(new Error(`${failures} tests failed.`));
 					} else {
-						c();
+						resolve();
 					}
 				});
 			} catch (err) {
 				console.error(err);
-				e(err);
+				reject(err);
 			}
 		});
 	});
