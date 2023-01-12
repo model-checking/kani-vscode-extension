956022c9a92cd44623cda3647e1deab9ea52310c
jaisnan <jaisnan@amazon.com>
2023-01-12 03:21:33 +0000

    Fix Kani Crashes for cases where there is an underlying import error

--- src/model/kaniBinaryRunner.ts
+++ src/model/kaniBinaryRunner.ts
@@ -19,6 +19,44 @@ const execAsync = exec;
  * @param args - arguments to Kani if provided
  * @returns verification status (i.e success or failure)
  */
+export async function runKaniHarnessInterface(
+	rsFile: string,
+	harnessName: string,
+	args?: number,
+): Promise<any> {
+	let harnessCommand = '';
+	if (args !== undefined || NaN) {
+		harnessCommand = `${KaniConstants.KaniExecutableName} ${rsFile} ${KaniArguments.harnessFlag} ${harnessName} ${KaniArguments.unwindFlag} ${args}`;
+	} else {
+		harnessCommand = `${KaniConstants.KaniExecutableName} ${rsFile} ${KaniArguments.harnessFlag} ${harnessName}`;
+	}
+	const kaniOutput = await catchOutput(harnessCommand);
+	if(kaniOutput == 2) {
+		const crateURI = getRootDir();
+		let harnessCommand = '';
+
+		if (args === undefined || NaN) {
+			harnessCommand = `cd ${crateURI} && ${KaniConstants.CargoKaniExecutableName} ${KaniArguments.harnessFlag} ${harnessName}`;
+		} else {
+			harnessCommand = `cd ${crateURI} && ${KaniConstants.CargoKaniExecutableName} ${KaniArguments.harnessFlag} ${harnessName} ${KaniArguments.unwindFlag} ${args}`;
+		}
+
+		// Cargo Kani Output
+		const kaniOutput: number = await catchOutput(harnessCommand, true);
+		return kaniOutput;
+	}
+	return kaniOutput;
+}
+
+
+/**
+ * Run Kani as a command line binary
+ *
+ * @param rsFile - Path to the file that is to be verified
+ * @param harnessName - name of the harness that is to be verified
+ * @param args - arguments to Kani if provided
+ * @returns verification status (i.e success or failure)
+ */
 export async function runKaniHarness(
 	rsFile: string,
 	harnessName: string,
@@ -30,7 +68,8 @@ export async function runKaniHarness(
 	} else {
 		harnessCommand = `${KaniConstants.KaniExecutableName} ${rsFile} ${KaniArguments.harnessFlag} ${harnessName}`;
 	}
-	console.log(harnessCommand);
+
+	// Kani Output
 	const kaniOutput = await catchOutput(harnessCommand);
 	return kaniOutput;
 }
@@ -57,7 +96,7 @@ export async function runCargoKaniTest(
 		harnessCommand = `cd ${crateURI} && ${KaniConstants.CargoKaniExecutableName} ${KaniArguments.testsFlag} ${KaniArguments.harnessFlag} ${harnessName} ${KaniArguments.unwindFlag} ${args}`;
 	}
 	if (failedCheck) {
-		const kaniOutput: KaniResponse = await captureFailedChecksForTests(harnessName, harnessCommand);
+		const kaniOutput: KaniResponse = await captureFailedChecksForProof(harnessName, harnessCommand);
 		return kaniOutput;
 	} else {
 		const kaniOutput: number = await catchOutput(harnessCommand);
@@ -110,7 +149,7 @@ export async function runCommandPure(command: string): Promise<void> {
 }
 
 // Run Kani and return the checks that failed as a lazy process
-async function captureFailedChecksForTests(
+async function captureFailedChecksForProof(
 	harnessName: string,
 	harnessCommand: string,
 ): Promise<KaniResponse> {
@@ -119,26 +158,39 @@ async function captureFailedChecksForTests(
 }
 
 // Run a command and capture the command line output into a string
-async function catchOutput(command: string): Promise<number> {
+async function catchOutput(command: string, cargoKaniMode: boolean = false): Promise<number> {
 	const process = await execLog(command);
 	// console.log(process);
 	return process;
 }
 
 // exectute the command as a command line argument
-async function execLog(command: string): Promise<number> {
+async function execLog(command: string, cargoKaniMode: boolean = false): Promise<number> {
 	return new Promise((resolve, reject) => {
 		execAsync(command, (error, stdout, stderr) => {
-			if (error) {
+			if(stderr && !stdout) {
+				if(cargoKaniMode) {
+					// stderr is an output stream that happens when there are no problems executing the kani command but kani itself throws an error due to (most likely)
+					// a rustc error or an unhandled kani error
+					vscode.window.showErrorMessage(`Kani Executable Crashed due to an underlying rustc error ->\n ${stderr}`);
+					reject();
+				}
+				else{
+					resolve(2)
+				}
+			}
+			else if (error) {
 				if (error.code === 1) {
 					// verification failed
 					// console.log("error code 1", stderr);
 					resolve(1);
 				} else {
-					vscode.window.showErrorMessage('Kani Executable Crashed');
+					// Error is an object created by nodejs created when nodejs cannot execute the command
+					vscode.window.showErrorMessage(`Kani Extension could not execute command ${command} due to error ->\n ${error}`);
 					reject();
 				}
-			} else {
+			}
+			else {
 				// verification successful
 				resolve(0);
 			}
