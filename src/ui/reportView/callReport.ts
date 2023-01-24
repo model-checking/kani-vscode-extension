5a5e32205976af19c3d3eb2c79ca01fa803af4a7
Jaisurya Nanduri <91620234+jaisnan@users.noreply.github.com>
2023-01-24 12:55:13 -0500

    Patch broken report finding (#20)

--- src/ui/reportView/callReport.ts
+++ src/ui/reportView/callReport.ts
@@ -3,16 +3,24 @@
 import * as fs from 'fs';
 import * as path from 'path';
 import process = require('process');
-import { setTimeout } from 'timers/promises';
 
 import * as vscode from 'vscode';
 
 import { KaniArguments, KaniConstants } from '../../constants';
 import { checkCargoExist, getRootDir } from '../../utils';
 
+const { exec } = require('child_process');
+const { promisify } = require('util');
+const execPromise = promisify(exec);
+
 interface htmlMetaData {
 	finalCommand: string;
 	searchDir: string;
+}
+
+interface visualizeOutput {
+	statusCode: number;
+	serverCommand: string;
 }
 
 /**
@@ -23,7 +31,7 @@ interface htmlMetaData {
  */
 export async function callViewerReport(
 	commandURI: string,
-	harnessObj: { harnessName: string; harnessFile: string, harnessType: boolean },
+	harnessObj: { harnessName: string; harnessFile: string; harnessType: boolean },
 ): Promise<void> {
 	let finalCommand: string = '';
 	let searchDir: string = '';
@@ -36,42 +44,43 @@ export async function callViewerReport(
 	// Detect source file
 	const terminal = vscode.window.activeTerminal ?? vscode.window.createTerminal();
 
+	// Generate the final visualize command for the supported platforms
 	if (platform === 'darwin' || platform == 'linux') {
-		const responseObject: htmlMetaData = createCommand(commandURI, harnessFile, harnessName, harnessType);
-		finalCommand = responseObject.finalCommand;
+		const responseObject: htmlMetaData = createCommand(
+			commandURI,
+			harnessFile,
+			harnessName,
+			harnessType,
+		);
+		const crateURI: string = getRootDir();
+		finalCommand = `cd ${crateURI} && ${responseObject.finalCommand}`;
 		searchDir = responseObject.searchDir;
 	}
 
-	terminal.sendText(finalCommand);
-
-	// Ask if they want to run this command
-	// First, fix this command
-	const filename: string = harnessName;
-
-	// Replace timeout with await for the final commmand to finish running
-	await setTimeout(5000);
-	const pythonCommand: string = `python3 -m http.server --directory`;
-
-	const filePath: string = findPath(searchDir, filename);
-	if (!filePath) {
-		console.error(' Could not find the filepath for the report ');
+	// Wait for the the visualize command to finish generating the report
+	const processOutput: visualizeOutput = await runVisualizeCommand(finalCommand, harnessName);
+	if (processOutput.statusCode == 1) {
+		// Could not run the visualize command, throw an error
+		vscode.window.showErrorMessage('Could not generate report due to execution error');
+		return;
+	} else if (processOutput.statusCode == 2) {
+		// Could run the command, but the file generated could not be verified or was generated at wrong location
+		vscode.window.showErrorMessage('Could not verify report path, error from Kani');
+		return;
 	}
-	const HTMLfilePath: string = path.join(filePath, 'html');
 
-	// const url = 'http://stackoverflow.com';
-	// require('child_process').exec(`open ${url}`);
-
-	// const HTMLfilePathFull = path.join(HTMLfilePath, 'index.html');
-	const fullReportCommand: string = pythonCommand + ` ` + HTMLfilePath;
-	terminal.sendText(fullReportCommand);
-
-	// If they do then, listen to the port yourself
-	// Kill Terminal after viewing the report
+	// Send the command to the user which asks them if they want to view the report on the browser
+	terminal.sendText(processOutput.serverCommand);
 	terminal.show();
 }
 
 // Check if cargo toml exists and create corresponding kani command
-function createCommand(commandURI: string, harnessFile: string, harnessName: string, harnessType: boolean): htmlMetaData {
+function createCommand(
+	commandURI: string,
+	harnessFile: string,
+	harnessName: string,
+	harnessType: boolean,
+): htmlMetaData {
 	// Check if cargo toml exists
 	const isCargo = checkCargoExist();
 	let finalCommand: string = '';
@@ -82,7 +91,7 @@ function createCommand(commandURI: string, harnessFile: string, harnessName: str
 		finalCommand = `${command} ${harnessFile} --harness ${harnessName} --visualize`;
 		searchDir = path.join(getRootDir());
 	} else {
-		if(harnessType) {
+		if (harnessType) {
 			const command: string = commandURI === 'Kani.runViewerReport' ? 'cargo kani' : 'kani';
 			finalCommand = `${command} --harness ${harnessName} --visualize`;
 			searchDir = path.join(getRootDir(), 'target');
@@ -95,22 +104,74 @@ function createCommand(commandURI: string, harnessFile: string, harnessName: str
 	return { finalCommand, searchDir };
 }
 
-// 	Find the path of the report from the harness name
-function findPath(dir: string, filename: string): string {
-	const files: string[] = fs.readdirSync(dir);
+/**
+ * Run the visualize command to generate the report, parse the output to return the python server command and status
+ *
+ * @param command - the cargo kani | kani command to run --visualize
+ * @returns - A promise of the python command and the status code; Promise<visualizeOutput>
+ */
+async function runVisualizeCommand(command: string, harnessName: string): Promise<visualizeOutput> {
+	try {
+		vscode.window.showInformationMessage(`Generating viewer report for ${harnessName}`);
+		const { stdout, stderr } = await execPromise(command);
+		const serveReportCommand: string = await parseReportOutput(stdout);
+		if (serveReportCommand === '') {
+			return { statusCode: 2, serverCommand: '' };
+		}
+		console.error(`stderr: ${stderr}`);
 
-	for (const file of files) {
-		const filePath: string = path.join(dir, file);
+		return { statusCode: 0, serverCommand: serveReportCommand };
+	} catch (error) {
+		console.error(`exec error: ${error}`);
+		return { statusCode: 1, serverCommand: '' };
+	}
+}
 
-		if (fs.statSync(filePath).isDirectory()) {
-			console.log(filePath);
-			if (file.includes(filename)) {
-				return filePath;
+/**
+ * Search for the python command contained in Kani's output and throw it onto the user's terminal
+ *
+ * @param stdout - kani stdout output that contains the full python command to be run by the terminal
+ * @returns - python command that looks like "python3 -m http.server --path path"
+ */
+async function parseReportOutput(stdout: string): Promise<string> {
+	const kaniOutput: string = stdout;
+	const kaniOutputArray: string[] = kaniOutput.split('\n');
+	const searchString: string = 'python3';
+
+	for (const outputString of kaniOutputArray) {
+		if (outputString.includes(searchString)) {
+			const command: string = outputString.split(searchString)[1];
+
+			// Check if the HTML path that is to be served to the user exists as expected
+			const filePathExists: boolean = await checkPathExists(command);
+			if (!filePathExists) {
+				return '';
 			}
-		} else {
-			continue;
+			return searchString + command;
 		}
 	}
 
+	// No command found from Kani
 	return '';
+}
+
+// Util function to find the path of the report from the harness name
+async function checkPathExists(serverCommand: string): Promise<boolean> {
+	// Kani returns a structured string, using that structure
+	// to find the filepath embedded in the command string
+	const lastSlashIndex: number = serverCommand.lastIndexOf('/');
+
+	if (lastSlashIndex !== -1) {
+		const match: string = serverCommand.substring(lastSlashIndex);
+		if (match) {
+			// Even if the path can be extracted from Kani's output, it's not
+			// necessary that the html file was created, so this needs to be checked
+			const exists: boolean = fs.existsSync(match[0]);
+			return exists;
+		} else {
+			return false;
+		}
+	} else {
+		return false;
+	}
 }
