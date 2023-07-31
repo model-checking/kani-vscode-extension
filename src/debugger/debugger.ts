96432fdf85db465a0f31e783db70d3a54a6bdfd6
jaisnan <jaisnan@amazon.com>
2023-07-31 13:05:47 -0400

    Fix JSON typo

--- src/debugger/debugger.ts
+++ src/debugger/debugger.ts
@@ -45,11 +45,11 @@ async function getBinaryPath(): Promise<string | undefined> {
 		const output = execFileSync(kaniBinaryPath, commandSplit.args, options);
 		const outputString = output.toString();
 		const lines = outputString.trim().split('\n');
-		// Remove version string before json parsing
+		// Remove version string before JSON parsing
 		// NOTE: This is a temporary patch till <https://github.com/model-checking/kani/issues/2649> is fixed.
 		lines.shift();
 
-		// Parse json objects from response
+		// Parse JSON objects from response
 		const jsonMessages = lines.map((line: string) => JSON.parse(line));
 
 		/*
